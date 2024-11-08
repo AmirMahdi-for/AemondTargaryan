59f76a66b1b8c8f88b198e74831560b7fc94bc73
AmirMahdi-for <amirmahdifor@gmail.com>
2024-11-08 11:48:19 -0800

    fit: solve bug for run project

--- internal/config/load.go
+++ internal/config/load.go
@@ -2,9 +2,14 @@ package config
 
 import (
 	"fmt"
-	"github.com/knadh/koanf/v2"
 	"log"
 	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/fatih/structs"
+	"github.com/knadh/koanf/providers/confmap"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/v2"
 )
 
 const (
@@ -19,10 +24,12 @@ const (
 func Load(print bool) *Config {
 	k := koanf.New(delimeter)
 
-	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+	// بارگذاری تنظیمات پیش‌فرض
+	if err := k.Load(confmap.Provider(structs.Map(Default()), delimeter), nil); err != nil {
 		log.Fatalf("error loading default: %s", err)
 	}
 
+	// بارگذاری متغیرهای محیطی
 	if err := loadEnv(k); err != nil {
 		log.Printf("error loading environment variables: %v", err)
 	}
@@ -30,10 +37,12 @@ func Load(print bool) *Config {
 	config := Config{}
 	var tag = koanf.UnmarshalConf{Tag: tagName}
 
+	// بازگشایی تنظیمات به ساختار Config
 	if err := k.UnmarshalWithConf("", &config, tag); err != nil {
 		log.Fatalf("error unmarshalling config: %v", err)
 	}
 
+	// چاپ تنظیمات در صورت نیاز
 	if print {
 		log.Printf("%s\n%v\n%s", upTemplate, spew.Sdump(config), bottomTemplate)
 	}
@@ -42,11 +51,13 @@ func Load(print bool) *Config {
 }
 
 func loadEnv(k *koanf.Koanf) error {
+	// تابع تبدیل نام متغیرهای محیطی
 	callback := func(source string) string {
 		base := strings.ToLower(strings.TrimPrefix(source, envPrefix))
 		return strings.ReplaceAll(base, delimeter, seperator)
 	}
 
+	// بارگذاری متغیرهای محیطی با استفاده از Provider
 	if err := k.Load(env.Provider(envPrefix, delimeter, callback), nil); err != nil {
 		return fmt.Errorf("error loading environment variables: %s", err)
 	}
